fe91e327c9711aecf8d29b710b7047cd935457f7
Lars Rosenquist <lrosenquist@pivotal.io>
2019-10-21 13:15:41 +0200

    Added discovery clients

--- 04-service-registry/src/main/java/io/pivotal/workshops/cnd/serviceregistry/ServiceRegistryDemoApplication.java
+++ 04-service-registry/src/main/java/io/pivotal/workshops/cnd/serviceregistry/ServiceRegistryDemoApplication.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceRegistryDemoApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ServiceRegistryDemoApplication.class);
--- fortune-ui/src/main/java/io/pivotal/workshops/cnd/fortuneui/FortuneUiApplication.java
+++ fortune-ui/src/main/java/io/pivotal/workshops/cnd/fortuneui/FortuneUiApplication.java
@@ -2,11 +2,13 @@ package io.pivotal.workshops.cnd.fortuneui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FortuneUiApplication {
 
 	@Bean
