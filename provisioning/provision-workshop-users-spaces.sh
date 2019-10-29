2e7190552ed987f77abcb7363a84e7bef5758209
David Caron <dcaron@xs4all.nl>
2019-10-29 16:01:45 +0100

    add user provisioning script

---
+++ provisioning/provision-workshop-users-spaces.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERSFILE=users.txt
+DEFAULT_PASSWORD=`cat password.txt`
+
+WORKSHOP_ORG=workshop
+
+cf target -o ${WORKSHOP_ORG}
+
+for user in `cat ${USERSFILE}`
+do
+    SPACENAME=`echo ${user} | awk -F"@" '{print $1}'`
+    echo "Creating user: ${user}"
+    cf create-user ${user} ${DEFAULT_PASSWORD}
+    echo "Creating space: ${SPACENAME}"
+    cf create-space ${SPACENAME}
+    cf set-space-role ${user} ${WORKSHOP_ORG} ${SPACENAME} SpaceDeveloper
+    cf set-space-role ${user} ${WORKSHOP_ORG} ${SPACENAME} SpaceManager
+    #cf set-space-role ${user} ${WORKSHOP_ORG} workshop SpaceDeveloper
+done
